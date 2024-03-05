c0b829dd07ad45f6506e4ffb40cecc0a0e313aa1
sebas-rom <gabrielsebastian.romero@gmail.com>
2024-03-04 19:40:41 -0500

    35

--- main.py
+++ main.py
@@ -83,9 +83,9 @@ def bat_algorithm(dataset, num_iterations=2, num_bats=10, subset_size=70, alpha=
 
 
 #Ussage
-iterations = 5 
-bat_number = 10 
-subset_size = 70 
+iterations = 150 
+bat_number = 5 
+subset_size = 35 
 best_subsets = []
 for _ in range(5):
     best_subset, best_fitness = bat_algorithm(dataset, iterations, bat_number,subset_size)
